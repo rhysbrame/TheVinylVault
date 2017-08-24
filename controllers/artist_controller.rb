32241a777696482f39a67734b83b96ea0b7b2378
rhysbrame <rhysbrame@gmail.com>
2017-08-24 17:15:36 +0100

    added routes for artist controller

--- controllers/artist_controller.rb
+++ controllers/artist_controller.rb
@@ -8,3 +8,35 @@ get '/artists' do
   @artists = Artist.all()
   erb(:'artists/index')
 end
+
+get '/artists/new' do
+  erb ( :'artists/new')
+end
+
+post '/artists' do
+  @artist = Artist.new(params)
+  @artist.save
+  redirect '/artists'
+end
+
+get '/artists/:id' do
+  @artist = Artist.find(params[:id])
+  erb ( :'artists/show' )
+end
+
+get '/artists/:id/edit' do
+  @artist = Artist.find(params[:id])
+  erb ( :'artists/edit')
+end
+
+post '/artists/:id' do
+  @artist = Artist.new(params)
+  @artist.update
+  erb ( :'artists/show' )
+end
+
+post '/artists/:id/delete' do
+  @artist = Artist.find(params[:id])
+  @artist.delete()
+  redirect '/artists'
+end
