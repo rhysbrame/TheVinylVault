aad95e166cbf5dc6e54912844be711e4b545e5d7
rhysbrame <rhysbrame@gmail.com>
2017-08-24 17:25:44 +0100

    added the routes for the genre controller

--- controllers/genre_controller.rb
+++ controllers/genre_controller.rb
@@ -8,3 +8,35 @@ get '/genres' do
   @genres = Genre.all()
   erb(:'genres/index')
 end
+
+get '/genres/new' do
+  erb ( :'genres/new')
+end
+
+post '/genres' do
+  @genre = Genre.new(params)
+  @genre.save
+  redirect '/genres'
+end
+
+get '/genres/:id' do
+  @genre = Genre.find(params[:id])
+  erb ( :'genres/show' )
+end
+
+get '/genres/:id/edit' do
+  @genre = Genre.find(params[:id])
+  erb ( :'genres/edit')
+end
+
+post '/genres/:id' do
+  @genre = Genre.new(params)
+  @genre.update
+  erb ( :'genres/show' )
+end
+
+post '/genres/:id/delete' do
+  @genre = Genre.find(params[:id])
+  @genre.delete()
+  redirect '/genre'
+end
